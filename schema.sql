477e05c1a5302d3d83ffce14d332e15f242f7016
Topash15 <alextopash@gmail.com>
2021-09-09 22:10:14 -0400

    added inquirer questions

--- schema.sql
+++ schema.sql
@@ -1,16 +1,20 @@
--- Department
--- id: INT PRIMARY KEY
--- name: VARCHAR(30) to hold department name
 
--- Role
--- id: INT PRIMARY KEY
--- title: VARCHAR(30) to hold role title
--- salary: DECIMAL to hold role salary
--- department_id: INT to hold reference to department role belongs to
+CREATE TABLE Department (
+    id INT PRIMARY KEY,
+    name VARCHAR(30)
+)
 
--- Employee
--- id: INT PRIMARY KEY
--- first_name: VARCHAR(30) to hold employee first name
--- last_name: VARCHAR(30) to hold employee last name
--- role_id: INT to hold reference to employee role
--- manager_id: INT to hold reference to another employee that is manager of the current employee. This field might be null if the employee has no manager.
+CREATE TABLE Role (
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+)
+
+CREATE TABLE Employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+)
